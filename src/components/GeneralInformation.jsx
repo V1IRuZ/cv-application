d040f29294a0cedb8be8ae98d229d7041c494a87
Esa <esa-pitkanen@hotmail.com>
2026-01-10 10:36:42 +0200

    feat: store input changes in a state variable

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,15 +1,15 @@
 import "../styles/GeneralInformation.css";
 
-const Input = ({ className, forId, label, type }) => {
+const Input = ({ className, forId, label, type, onChange }) => {
   return (
     <div className={className}>
       <label htmlFor={forId}>{label}</label>
-      <input type={type} id={forId} />
+      <input type={type} id={forId} onChange={onChange}/>
     </div>
   );
 };
 
-export default function GeneralInformation() {
+export default function GeneralInformation({personData, setPersonData}) {
   return (
     <section className="general-information">
       <h1>General Information</h1>
@@ -19,12 +19,14 @@ export default function GeneralInformation() {
           forId={"firstName"}
           label={"First name"}
           type={"text"}
+          onChange={(e) => setPersonData({...personData, firstName: e.target.value})}
         />
         <Input
           className={"last-name"}
           forId={"lastName"}
           label={"Last name"}
           type={"text"}
+          onChange={(e) => setPersonData({...personData, lastName: e.target.value})}
         />
       </div>
       <div className="contact">
@@ -33,12 +35,14 @@ export default function GeneralInformation() {
           forId={"phone"}
           label={"Phone number"}
           type={"tel"}
+          onChange={(e) => setPersonData({...personData, phone: e.target.value})}
         />
         <Input
           className={"email"}
           forId={"email"}
           label={"E-Mail"}
           type={"email"}
+          onChange={(e) => setPersonData({...personData, email: e.target.value})}
         />
       </div>
     </section>
