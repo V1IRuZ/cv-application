435fad5a3908ab6242ec38877c49de2fa2aaccc6
Esa <esa-pitkanen@hotmail.com>
2026-01-10 14:17:53 +0200

    feat: add a resume component to display information

---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+export default function Resume({ personData, setFormIsActive }) {
+  const name = `${personData.firstName} ${personData.lastName}`;
+
+  return (
+    <div className="resume">
+      <div>
+        <p>{name}</p>
+        <p>{personData.phone}</p>
+        <p>{personData.email}</p>
+      </div>
+      <button onClick={() => setFormIsActive(true)}>Edit</button>
+    </div>
+  );
+}
