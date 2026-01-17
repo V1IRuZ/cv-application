ae48c987617e71e5ebcac83bbe0a61cdc532849c
Esa <esa-pitkanen@hotmail.com>
2026-01-17 15:19:37 +0200

    feat: add Language component
    - LanguageInput (child component) has inputs for language and skill level
    - onChange event controls inputs
    - Add button to add new languages to resume
    - Remove button to delete language skills

---
+++ src/components/Language.jsx
@@ -0,0 +1,85 @@
+function LanguageInput({ data, index, onRemove, setData }) {
+  const languageNumber = index + 1;
+
+  return (
+    <div className="language-card">
+      <h2>Language #{languageNumber}</h2>
+      <div className="language-skill">
+        <div className="input">
+          <label htmlFor={`language-${data.id}`}>Language</label>
+          <input
+            type="text"
+            id={`language-${data.id}`}
+            value={data.skill}
+            onChange={(e) =>
+              setData((prevData) =>
+                prevData.map((item) =>
+                  item.id === data.id
+                    ? { ...item, skill: e.target.value }
+                    : item
+                )
+              )
+            }
+          />
+        </div>
+        <div className="input">
+          <label htmlFor={`rating-${data.id}`}>Skill level</label>
+          <input
+            type="text"
+            id={`rating-${data.id}`}
+            value={data.rating}
+            onChange={(e) =>
+              setData((prevData) =>
+                prevData.map((item) =>
+                  item.id === data.id
+                    ? { ...item, rating: e.target.value }
+                    : item
+                )
+              )
+            }
+          />
+        </div>
+      </div>
+      <div className="remove">
+        <button className="remove-btn" onClick={() => onRemove(data.id)}>
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function Language({ languageSkills, setLanguageSkills }) {
+  const onAdd = (e) => {
+    e.preventDefault();
+    setLanguageSkills((prev) => [
+      ...prev,
+      { skill: "", rating: "", id: crypto.randomUUID() },
+    ]);
+  };
+
+  const onRemove = (id) => {
+    const newLanguages = languageSkills.filter(
+      (language) => language.id !== id
+    );
+
+    setLanguageSkills(newLanguages);
+  };
+
+  return (
+    <section className="language">
+      <div className="header">
+        <h1>Languages</h1>
+        <button onClick={onAdd}>Add new...</button>
+      </div>
+      {languageSkills.map((language, index) => (
+        <LanguageInput
+          data={language}
+          index={index}
+          onRemove={onRemove}
+          setData={setLanguageSkills}
+        />
+      ))}
+    </section>
+  );
+}
