cbce82f7904af7741b2c1d56378e7144fa393657
Esa <esa-pitkanen@hotmail.com>
2026-01-17 10:28:56 +0200

    feat: add new Jobs component

---
+++ src/components/Jobs.jsx
@@ -0,0 +1,14 @@
+import "../styles/Experience.css";
+import Experience from "./Experience";
+
+export default function Jobs({ jobsData, setJobsData }) {
+  return (
+    <section className="jobs">
+      <Experience
+        sectionHeader="Practical experience"
+        experienceData={jobsData}
+        setExperienceData={setJobsData}
+      />
+    </section>
+  );
+}
